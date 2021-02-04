b067bef8e3a6038d1ef8e6b78630c4a71a107052
Edward Amor <edward.amor3@gmail.com>
2021-02-04 13:12:13 -0500

    feat: add empty DiamondCut facet contract

---
+++ contracts/facets/DiamondCut.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.4;
+pragma abicoder v2;
+
+import {IDiamondCut} from "../../interfaces/ERC2535/IDiamondCut.sol";
+
+contract DiamondCut is IDiamondCut {}
