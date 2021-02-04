eb067cf66aa46484680792d384cfaf518ebbb67a
Edward Amor <edward.amor3@gmail.com>
2021-02-04 16:45:14 -0500

    fix: add type and location to variable declares

--- contracts/facets/DiamondLoupe.sol
+++ contracts/facets/DiamondLoupe.sol
@@ -4,8 +4,12 @@ pragma abicoder v2;
 
 import {IDiamondLoupe} from "../../interfaces/ERC2535/IDiamondLoupe.sol";
 import {LibDiamond} from "../libraries/LibDiamond.sol";
+import {EnumerableSet} from "openzeppelin/contracts/utils/EnumerableSet.sol";
 
 contract DiamondLoupe is IDiamondLoupe {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+
     function facets()
         external
         view
@@ -13,9 +17,9 @@ contract DiamondLoupe is IDiamondLoupe {
         returns (IDiamondLoupe.Facet[] memory facets_)
     {
         // load the diamond storage
-        LibDiamond.DiamondStorage ds = LibDiamond.diamondStorage();
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         // get the diamond facets
-        _facets = ds.facets;
+        EnumerableSet.AddressSet storage _facets = ds.facets;
         // initialize size of the return
         facets_ = new IDiamondLoupe.Facet[](_facets.length());
         for (uint256 facetIndex; facetIndex < _facets.length(); facetIndex++) {
@@ -36,12 +40,13 @@ contract DiamondLoupe is IDiamondLoupe {
         returns (bytes4[] memory facetFunctionSelectors_)
     {
         // load the diamond storage
-        LibDiamond.DiamondStorage ds = LibDiamond.diamondStorage();
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         // require _facet address is in the array of diamond facets
         require(ds.facets.contains(_facet)); // dev: Invalid facet address
 
-        // get the known function selectors for facet_
-        selectors = ds.facetAddressToFunctionSelectors[facet_];
+        // get the known function selectors for _facet
+        EnumerableSet.Bytes32Set storage selectors =
+            ds.facetAddressToFunctionSelectors[_facet];
         // use library function to quickly make the return array
         facetFunctionSelectors_ = LibDiamond.selectorsBytes32ToBytes4Array(
             selectors
@@ -55,9 +60,9 @@ contract DiamondLoupe is IDiamondLoupe {
         returns (address[] memory facetAddresses_)
     {
         // load the diamond storage
-        LibDiamond.DiamondStorage ds = LibDiamond.diamondStorage();
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         // the enumerable set of facets
-        _facets = ds.facets;
+        EnumerableSet.AddressSet storage _facets = ds.facets;
         // initialize size of the return array
         facetAddresses_ = new address[](ds.facets.length());
         for (uint256 facetIndex; facetIndex < _facets.length(); facetIndex++) {
@@ -73,7 +78,7 @@ contract DiamondLoupe is IDiamondLoupe {
         returns (address facetAddress_)
     {
         // load the diamond storage
-        LibDiamond.DiamondStorage ds = LibDiamond.diamondStorage();
-        facetAddress_ = selectorToFacetAddress[_functionSelector];
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        facetAddress_ = ds.selectorToFacetAddress[_functionSelector];
     }
 }
