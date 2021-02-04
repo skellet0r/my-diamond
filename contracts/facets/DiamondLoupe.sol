c28d385d31468937165ea6439dc352e80a057e8c
Edward Amor <edward.amor3@gmail.com>
2021-02-04 13:11:49 -0500

    feat: add empty DiamondLoupe facet contract

---
+++ contracts/facets/DiamondLoupe.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.4;
+pragma abicoder v2;
+
+import {IDiamondLoupe} from "../../interfaces/ERC2535/IDiamondLoupe.sol";
+
+contract DiamondLoupe is IDiamondLoupe {}
