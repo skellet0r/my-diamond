3df9388c6427adbb53f91c74e917732c786a816e
Edward Amor <edward.amor3@gmail.com>
2021-02-04 14:04:48 -0500

    feat: add mock contract

---
+++ contracts/mocks/MockContract.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.4;
+pragma abicoder v2;
+
+contract MockContract {
+    constructor() public {
+        setVal(0);
+    }
+
+    function main() external returns (uint256 val_) {
+        setVal(100);
+    }
+
+    function getVal() external view returns (uint256 val_) {
+        bytes32 position = keccak256("mock.contract.value");
+        assembly {
+            val_ := sload(position)
+        }
+    }
+
+    function setVal(uint256 val_) internal {
+        bytes32 position = keccak256("mock.contract.value");
+        assembly {
+            sstore(position, val_)
+        }
+    }
+}
