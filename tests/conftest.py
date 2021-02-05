bcfb0b2125735e7147d68a0c1a82ce1c2aee5fa8
Edward Amor <edward.amor3@gmail.com>
2021-02-04 23:56:56 -0500

    feat: migrate fixtures into conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import pytest
+from brownie import Contract
+from brownie.convert import to_address
+
+
+class FacetCutAction:
+
+    Add = 0
+    Replace = 1
+    Remove = 2
 
 
 @pytest.fixture(scope="module")
@@ -17,6 +26,54 @@ def barry(accounts):
 def charlie(accounts):
     """Get the third available account."""
     return accounts[2]
+
+
+@pytest.fixture(scope="module")
+def zero_address():
+    return to_address("0x0000000000000000000000000000000000000000")
+
+
+@pytest.fixture(scope="module")
+def diamondcut(adam, DiamondCut):
+    """Deploy the DiamondCut contract."""
+    return adam.deploy(DiamondCut)
+
+
+@pytest.fixture(scope="module")
+def diamondloupe(adam, DiamondLoupe):
+    """Deploy the DiamondLoupe contract."""
+    return adam.deploy(DiamondLoupe)
+
+
+@pytest.fixture(scope="module")
+def diamond(adam, diamondcut, diamondloupe, Diamond, DiamondCut, DiamondLoupe):
+    """Deploy the Diamond contract, and initialize with DiamondCut and DiamondLoupe."""
+    facet_cuts = [
+        (diamondcut.address, FacetCutAction.Add, list(DiamondCut.signatures.values())),
+        (
+            diamondloupe.address,
+            FacetCutAction.Add,
+            list(DiamondLoupe.signatures.values()),
+        ),
+    ]
+    return adam.deploy(Diamond, facet_cuts)
+
+
+@pytest.fixture(scope="module")
+def cut(diamond, DiamondCut):
+    """Diamond contract with cut functions available."""
+    return Contract.from_abi("Diamond Cut", diamond.address, DiamondCut.abi)
+
+
+@pytest.fixture(scope="module")
+def loupe(diamond, DiamondLoupe):
+    """Diamond contract with loupe functions available."""
+    return Contract.from_abi("Diamond Loupe", diamond.address, DiamondLoupe.abi)
+
+
+@pytest.fixture(scope="module")
+def facet_cut_actions():
+    return FacetCutAction
 
 
 @pytest.fixture(autouse=True)
