553f730eec1ade95e2ebed3f62ae656811edcd69
Edward Amor <edward.amor3@gmail.com>
2021-02-04 19:42:21 -0500

    fix: call LibDiamond diamondCut method

--- contracts/facets/DiamondCut.sol
+++ contracts/facets/DiamondCut.sol
@@ -11,30 +11,7 @@ contract DiamondCut is IDiamondCut {
         address _init,
         bytes calldata _calldata
     ) external override {
-        // loop through all the cuts
-        for (
-            uint256 facetIndex;
-            facetIndex < _diamondCut.length;
-            facetIndex++
-        ) {
-            // what action is being performed
-            IDiamondCut.FacetCutAction action = _diamondCut[facetIndex].action;
-            if (action == IDiamondCut.FacetCutAction.Add) {
-                // add the functions
-                LibDiamond.addFunctions(
-                    _diamondCut[facetIndex].facetAddress,
-                    _diamondCut[facetIndex].functionSelectors
-                );
-            } else if (action == IDiamondCut.FacetCutAction.Replace) {
-                // do something
-            } else if (action == IDiamondCut.FacetCutAction.Remove) {
-                // do something
-            } else {
-                // revert because the wrong action was given
-                revert(); // dev: Incorrect FacetCutAction
-            }
-        }
-        emit DiamondCut(_diamondCut, _init, _calldata);
-        // initialize the diamond
+        // Add/Replace/Remove functions
+        LibDiamond.diamondCut(_diamondCut, _init, _calldata);
     }
 }
